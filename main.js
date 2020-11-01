c874965e8c0e78c3d31741335e1446e174b07e9f
unknown <cuonghaha43@gmail.com>
2020-11-01 14:31:09 +0700

    add file

---
+++ main.js
@@ -0,0 +1,26 @@
+function drop() {
+    document.getElementById("list").classList.toggle("show");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("content-1-banner");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+}
